b85c078a91a07bf6f53241725300d97f41b24d59
Misel Evtimov <misel.evtimov@gmail.com>
2020-11-05 20:13:32 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
 # Write your methods here
+def loop_message_five_times(message)
+count = 0
+while count < 5 do
+  puts message
+  count += 1
+end
+end
+
+def loop_message_n_times(message, integer)
+count = 0
+until count = integer
+  puts message
+  count += 1
+end
+end
